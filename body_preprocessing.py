52376a31bb2dbf375d54a3850721de8d2b9a92cd
max <max.kusnadi@gmail.com>
2017-02-21 22:57:30 +0800

    add popular questions of the year

--- body_preprocessing.py
+++ body_preprocessing.py
@@ -1,5 +1,6 @@
 
 import pandas as pd
+import logging
 from bs4 import BeautifulSoup
 
 ORIGINAL_DATASET = input("Enter the path of ur original csv path: ")
@@ -7,8 +8,14 @@ TARGET_DATASET = input("Enter the path of ur target csv path: ")
 
 data = pd.read_csv(ORIGINAL_DATASET, encoding='latin1')
 
+count_string = 0
+count_code = 0
+
 
 def get_string(text):
+    global count_string
+    logging.debug("Processing get_string Job:{}".format(count_string))
+    count_string += 1
     soup = BeautifulSoup(text, 'html.parser')
     lst = []
     for p in soup.find_all('p'):
@@ -17,6 +24,9 @@ def get_string(text):
 
 
 def is_code_present(text):
+    global count_code
+    logging.debug("Processing get_string Job:{}".format(count_code))
+    count_code += 1
     soup = BeautifulSoup(text, 'html.parser')
     return 1 if soup.pre else 0
 
